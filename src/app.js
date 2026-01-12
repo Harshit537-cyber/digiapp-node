4fbef38fabefa354c845d2bd9950f1c9b7b8ed79
Harshit <harshit.kumar@pearlorganisation.com>
2026-01-12 11:02:06 +0530

    "ok-Fixed"

--- src/app.js
+++ src/app.js
@@ -1,38 +1,28 @@
 const express = require("express");
 const connectDB = require("./config/db");
+
 const userRoutes = require("./routes/user.routes");
-const bloodRequestRoutes  = require("../../DigiApp/src/routes/bloodRequest.routes");
-const trustedContactRoutes = require('./routes/trustedContact.routes');
+const bloodRequestRoutes = require("./routes/bloodRequest.routes");
+const trustedContactRoutes = require("./routes/trustedContact.routes");
 const jobRoutes = require("./routes/job.routes");
-const businessRoutes = require('../src/routes/business.routes'); 
-const itemRoutes = require('./routes/item.routes'); 
-const adminRoutes = require('./admin/routes/adminRoutes');
-const app = express();
+const businessRoutes = require("./routes/business.routes");
+const itemRoutes = require("./routes/item.routes");
+const adminRoutes = require("./admin/routes/adminRoutes");
+
 require("dotenv").config();
 
+const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
- 
-
 app.use("/api/user", userRoutes);
-
 app.use("/api/blood-request", bloodRequestRoutes);
-
-app.use("/api/job", jobRoutes); 
-
-app.use('/api/trusted-contact', trustedContactRoutes);
-
-app.use('/api/business', businessRoutes); 
-
-app.use('/api/items', itemRoutes); 
-
-
-// -------------------------Admin Api---------------------
-console.log("Admin routes loading..."); // Ye terminal mein dikhna chahiye server start hote waqt
-app.use('/api/admin', adminRoutes);
-
+app.use("/api/job", jobRoutes);
+app.use("/api/trusted-contact", trustedContactRoutes);
+app.use("/api/business", businessRoutes);
+app.use("/api/items", itemRoutes);
+app.use("/api/admin", adminRoutes);
 
 module.exports = app;
