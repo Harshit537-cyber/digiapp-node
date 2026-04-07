757bebabe9aa993665fbfa92b3fdd38dbac6208b
Gaurav Saini <gaurav-saini@pearlorganisation.com>
2026-04-07 12:38:48 +0530

    api

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,7 +2,7 @@ const Category = require('../admin/models/Category');
 
 exports.getAppCategories = async (req, res) => {
     try {
-        const categories = await Category.find({ status: true });
+        const categories = await Category.distinct("category", { status: true });
         res.status(200).json(categories);
     } catch (error) {
         res.status(500).json({ error: error.message });
