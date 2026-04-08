00baba7e902a7fa44450c276bbbf65da89b70c5c
Gaurav Saini <gaurav-saini@pearlorganisation.com>
2026-04-08 16:54:24 +0530

    api

--- src/admin/routes/itemRoutes.js
+++ src/admin/routes/itemRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const verifyAdmin = require("../middlewares/adminAuth");
-const { getAllItems, updateItem } = require("../controllers/itemsController");
+const { getAllItems, updateItem, deleteItem } = require("../controllers/itemsController");
 
 const router = express.Router();
 
 router.get("/Items", verifyAdmin, getAllItems);
 
 router.put("/update/:id", verifyAdmin, updateItem);
+
+router.delete("/delete/:id", verifyAdmin, deleteItem);
 module.exports = router;
