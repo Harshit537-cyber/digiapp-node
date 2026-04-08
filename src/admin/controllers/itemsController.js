e3ae3e8d12a188e3f60b21c4abefcf092682f558
Gaurav Saini <gaurav-saini@pearlorganisation.com>
2026-04-08 16:44:16 +0530

    api

--- src/admin/controllers/itemsController.js
+++ src/admin/controllers/itemsController.js
@@ -67,4 +67,32 @@ const updateItem = async (req, res) => {
     }
 };
 
-module.exports = {getAllItems, updateItem};
+
+const deleteItem = async (req, res) => {
+    try {
+        const item = await Item.findByIdAndDelete(req.params.id);
+
+        
+        if (!item) {
+            return res.status(404).json({
+                success: false,
+                message: "Item not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Item deleted successfully"
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Error deleting item",
+            error: error.message
+        });
+    }
+};
+
+
+module.exports = {getAllItems, updateItem, deleteItem};
