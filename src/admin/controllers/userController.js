d43d61679117cc62da9a33ac61614557baa3e937
Gaurav Saini <gaurav-saini@pearlorganisation.com>
2026-04-10 15:39:53 +0530

    api

--- src/admin/controllers/userController.js
+++ src/admin/controllers/userController.js
@@ -6,12 +6,22 @@ const response = require("../../utils/response")
 
  const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({}).select("-token -fcmToken -__v");
+   const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+ const totalUsers = await User.countDocuments();
+    const users = await User.find({}).select("-token -fcmToken -__v")
+    .skip(skip)
+      .limit(limit);
 
     res.status(200).json({
       success: true,
+      currentPage: page,
+      totalPages: Math.ceil(totalUsers / limit),
       count: users.length,
       data: users,
+      totalUsers: totalUsers
     });
   } catch (error) {
     res.status(500).json({
